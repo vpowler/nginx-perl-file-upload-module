9af3d428243d5ac98155ea033b9326127b4430d9
vpowler <vayne.powler@gmail.com>
2023-02-21 18:50:51 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-sed -i "s/UPLOAD_DIR/$UPLOAD_DIR/g" /etc/nginx/nginx.conf
+#sed -i "s/UPLOAD_DIR/$UPLOAD_DIR/g" /etc/nginx/nginx.conf
 
-mkdir -p $UPLOAD_DIR
-chown nginx $UPLOAD_DIR
+echo $UPLOAD_DIR > /tmp/1
+
+#mkdir -p $UPLOAD_DIR
+#chown nginx $UPLOAD_DIR
 
 exec "$@"
